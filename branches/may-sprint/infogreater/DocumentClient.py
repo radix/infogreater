8f5cabb2a293808e12150ec4091c1f3afcfcb6a9
class30 <>
2005-06-01 02:44:06 -0400

    Suggestive of tests / implementation for Client. Doesn't actually pass tests.
    AB

--- branches/may-sprint/infogreater/DocumentClient.py
+++ branches/may-sprint/infogreater/DocumentClient.py
@@ -1,1 +1,11 @@
+from twisted.spread import pb
+from twisted.cred.credentials import UsernamePassword
+from twisted.internet import reactor
 
+class Client:
+    def __init__(self):
+        self.factory = pb.PBClientFactory()
+
+    def connect(self, hostname):
+        self.connector = reactor.connectTCP(hostname, pb.portno, self.factory)
+        return self.factory.login(UsernamePassword("guest", "guest")) 
