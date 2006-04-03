4bf1a820d9263055c0efaf799214523fa043a182
Christopher Armstrong <227068+radix@users.noreply.github.com>
2006-04-02 22:47:10 -0400

    rearrange to get rid of trunk/branches/tags.

--- branches/may-sprint/infogreater/test/test_framework.py
+++
@@ -1,8 +0,0 @@
-class DummyController(object):
-    def __init__(self):
-        self.canvas = DummyCanvas()
-        
-class DummyCanvas(object):
-    def put(*a):
-        pass
-
--- branches/xml-faceted-rewrite/infogreater/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-InfoGreater. INTEGRATE YOUR INFORMATION GREATLY.
-"""
